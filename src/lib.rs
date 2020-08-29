7bf919af7c142c741660b17788915f0b3dbc7cba
Terkwood <38859656+Terkwood@users.noreply.github.com>
2020-08-29 11:34:13 -0400

    Add index

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,3 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
-    }
-}
+#![recursion_limit = "1024"]
+extern crate serde_derive;
+extern crate serde_json;
